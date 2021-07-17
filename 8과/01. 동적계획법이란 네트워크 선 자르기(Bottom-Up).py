998fb4d5fe44bf5c99f31678df8509ca1b8e20f4
오준호 <37764288+highheaven15@users.noreply.github.com>
2021-07-17 22:59:25 +0900

    Update 01. 동적계획법이란 네트워크 선 자르기(Bottom-Up).py

--- 8과/01. 동적계획법이란 네트워크 선 자르기(Bottom-Up).py
+++ 8과/01. 동적계획법이란 네트워크 선 자르기(Bottom-Up).py
@@ -9,7 +9,7 @@ sys.stdin = open("input.txt", "rt")
 
 n=int(input())
 dy=[0]*(n+1)
-dy[1]=1 #직관적으로 알수있는 것들은 초기
+dy[1]=1 #직관적으로 알수있는 것들은 초기값 세팅
 dy[2]=2
 for i in range(3, n+1):
   dy[i]=dy[i-2]+dy[i-1]
